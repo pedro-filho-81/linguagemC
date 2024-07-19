b1579416e90c77b15b98903b5fcf09f7820dc711
PedroFilho <psfilho81@hotmail.com>
2024-07-19 18:19:12 -0300

    Update main.c

--- olamundo/main.c
+++ olamundo/main.c
@@ -1,8 +1,14 @@
+// bibliotecas padrão
 #include <stdio.h>
 #include <stdlib.h>
 
+//  função principal
 int main()
-{
+{ // abre o bloco
+   // imprime o que está entre aspas
     printf("Olá,mundo C!\n");
+    // pausa a execução do programa
+    system("pause");
+    // retorna zero, informando programa terminado com sucesso
     return 0;
-}
+} // encerra o bloco
