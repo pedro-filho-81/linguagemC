5e25aa0fe769baed4fb790da6a0fb81a0dce430f
PedroFilho <psfilho81@hotmail.com>
2024-07-20 10:59:54 -0300

    Create teste.c

---
+++ variaveis/teste.c
@@ -0,0 +1,16 @@
+// bibliotecas C
+#include <stdio.h> // para entrada e saída
+#include <stdlib.h> // para instrução system
+#include <locale.h> // para configurar a região
+
+int main()
+{ // inicio do bloco
+   setlocale(LC_ALL, "portuguese"); // configura para português do Brasil
+   printf("Variáveis:\n"); // exibe cabeçalho
+   int x; // declara a variável mas não define o valor
+   printf("x = %d%n", x); // imprime o valor de x
+   x = 5; // define o valor de x como sendo 5
+   printf("\nx = %d%n", x); // imprime o valor de x
+   system("pause"); // pausa o programa
+   return 0; // retorna zero, programa terminado com sucesso.
+} // end main
