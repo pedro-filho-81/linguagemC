3ff72037d5ef48436d7204bb119257378e75c4bb
PedroFilho <psfilho81@hotmail.com>
2024-07-21 16:30:19 -0300

    Create operador_or.c

---
+++ operadores/logicos/operador_OR/operador_or.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    printf("\nOPERADOR OU/OR/||\n");
+    printf("verdade || verdade  =  verdade\n");
+    printf("verdade || falso    =  verdade\n");
+    printf("falso   || verdade  =  verdade\n");
+    printf("falso   || falso    =  falso\n");
+} // end main
