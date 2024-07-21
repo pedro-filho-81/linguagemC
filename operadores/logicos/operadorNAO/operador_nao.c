6d09d1529e81cbe384145714763f5341c7fa1b61
PedroFilho <psfilho81@hotmail.com>
2024-07-21 16:30:33 -0300

    Create operador_nao.c

---
+++ operadores/logicos/operadorNAO/operador_nao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    printf("\nOPERADOR NÃO/NOT/ ! \n");
+    printf("!verdade  =  FALSO\n");
+    printf("!falso    =  VERDADE\n");
+} // end main
