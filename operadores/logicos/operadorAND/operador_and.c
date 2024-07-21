6f6de74747d4e2753a8739e95ebc6325d7f0640d
PedroFilho <psfilho81@hotmail.com>
2024-07-21 16:30:26 -0300

    Create operador_and.c

---
+++ operadores/logicos/operadorAND/operador_and.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    printf("\nOPERADOR E/AND/&&\n");
+    printf("verdade  &&  verdade  =  verdade\n");
+    printf("verdade  &&  falso    =  falso\n");
+    printf("falso    &&  verdade  =  falso\n");
+    printf("falso    &&  falso    =  falso\n");
+} // end main
