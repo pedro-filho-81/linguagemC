a4db00db9f9667c9c81eed96957a3a5eab0bb4b7
PedroFilho <psfilho81@hotmail.com>
2024-07-29 16:18:20 -0300

    Atualizações

---
+++ comoProgramarC/exemplos/cap02/exemplos/primeiroPrograma.c
@@ -0,0 +1,16 @@
+/*
+    1Figura 2.1: fig02_01.c
+    2 Primeiro programa em C
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 20). Edi��o do Kindle.
+*/
+#include <stdio.h>
+
+// fun��o principal
+int main()
+{
+    // imprima
+    printf("Bem vindo a linguagem C!" );
+
+    // fim programa
+    return 0;
+} // fim main
---
+++ comoProgramarC/exemplos/cap02/exemplos/umPrintVariasLinhas.c
@@ -0,0 +1,14 @@
+/*
+    /* Figura 2.4: fig02_04.c
+    2 Imprimindo v�rias linhas com uma �nica instru��o printf
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 23). Edi��o do Kindle.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Bem-vindo\na\nC!\n" );
+
+    return 0; // fim do programa
+} // fim de main
