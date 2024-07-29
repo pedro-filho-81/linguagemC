2f00c2866ca7fdc35ebbfe58dc303dad6e118ca8
PedroFilho <psfilho81@hotmail.com>
2024-07-29 16:11:17 -0300

    Create valorParaVariavel.c

---
+++ vetores_matrizes/exemplos/exp01/valorParaVariavel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    atribuindo um valor a varuável
+*/
+int main(void) {
+
+    float x = 10.00;
+    printf("%f\n", x);
+    x = 20.00;
+    printf("%f\n", x);
+    system("pause");
+    return 0;
+} // end 
